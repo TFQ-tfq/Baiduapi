76af736419c89478ebd2bfbc6e662c8bd5ddf297
24850 <tangfangquan@mbse.com.cn>
2024-03-10 17:05:49 +0800

    1111

--- baiduapi/run/run_alltest.py
+++ baiduapi/run/run_alltest.py
@@ -1,4 +1,5 @@
 import os
+
 if __name__ == '__main__':
     os.system('pytest -s -v ../testcase/test_open_baidu.py --alluredir=../report --clean-alluredir')
     os.system('allure generate ../report -o ../report-html --clean')
--- baiduapi/testcase/test_open_baidu.py
+++ baiduapi/testcase/test_open_baidu.py
@@ -1,9 +1,19 @@
 import pytest
 
-from baseapi.baseapi import openbaidu
+from baiduapi.baseapi.baseapi import openbaidu
+
 
 def test_open_baidu():
     result = openbaidu()
 
-# if __name__ == '__main__':
-#     pytest.main(['-vs'])
+def test_open_baidu01():
+    pass
+
+def test_open_baidu02():
+    pass
+
+def test_open_baidu03():
+    pass
+
+if __name__ == '__main__':
+    pytest.main(['-vs'])
